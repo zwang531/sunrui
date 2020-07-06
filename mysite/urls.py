fb2dd16278096eccaba7f24b708788b2fdff7e72
zongwei wang <megonwang@gmail.com>
2020-07-06 23:57:15 +0800

    remove other apps

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('myapp/', include('my_app.urls')),
-    path('login/', include('login.urls')),
+    # path('myapp/', include('my_app.urls')),
+    # path('login/', include('login.urls')),
     path('', include('sunrui.urls')),
     path('admin/', admin.site.urls)
 ]
