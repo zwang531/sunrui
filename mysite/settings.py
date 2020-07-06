6bcb957b10a59491a20f427878e18352fd8bb86a
zongwei wang <megonwang@gmail.com>
2020-07-07 00:00:19 +0800

    refactor

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,8 +32,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'login.apps.LoginConfig',
-    'my_app.apps.MyAppConfig',
     'mysite.apps.MyAdminConfig',  # 'django.contrib.admin',
     'sunrui.apps.SunruiConfig',
     'django.contrib.auth',
--- mysite/urls.py
+++ mysite/urls.py
@@ -17,8 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    # path('myapp/', include('my_app.urls')),
-    # path('login/', include('login.urls')),
     path('', include('sunrui.urls')),
     path('admin/', admin.site.urls)
 ]
